f25addf49e3b788e1417a155bce9d055bb602753
goekeca <goekeca@mail.uc.edu>
2017-02-07 04:13:22 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link href='https://fonts.googleapis.com/css?family=Raleway:600,400,500,300,200,400italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="./styles/stylesheets/main_styles.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+		<script src="./scripts/main_scripts.js"></script>
+		<title>Cooper Goeke</title>
+		<!--Favicons-->
+		<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+		<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#224cc5">
+		<meta name="msapplication-TileColor" content="#11308a">
+		<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+		<meta name="theme-color" content="#224cc5">
+	</head>
+	<body>
+		<div id="header_logo_wrapper">
+			<img id="header_logo_2" class="header_logo" src="./images/header_logo_2.svg">
+			<img id="header_logo_3" class="header_logo" src="./images/header_logo_3.svg">
+			<img id="header_logo_4" class="header_logo" src="./images/header_logo_4.svg">
+			<img id="header_logo_5" class="header_logo" src="./images/header_logo_5.svg">
+			<h1 class="hidden">Cooper<span>Goeke</span></h1>
+		</div>
+		<?php require('./html_partials/menu.html'); ?>
+		<?php require('./html_partials/side_navigation.html'); ?>
+		<?php require('./html_partials/page_5.html'); ?>
+		<?php require('./html_partials/page_4.html'); ?>
+		<?php require('./html_partials/page_3.html'); ?>
+		<?php require('./html_partials/page_2.html'); ?>
+		<?php require('./html_partials/page_1.html'); ?>
+	</body>
+</html>
---
+++ scripts/main_scripts.js
@@ -0,0 +1,241 @@
+var current_page = 0;
+var menu_open = false;
+var lock = false;
+var menu_lock = false;
+var max_pages = 5;
+var page_3_color = "#4774c5";
+var page_3_slide = 1;
+var page_3_max = 6;
+var page_4_slide = 1;
+var page_4_max = 5;
+
+function unlock(){
+	lock = false;
+}
+
+function toggleMenu(){
+	if( menu_lock ) return;
+	if( menu_open == true ){
+		menu_open = false;
+		menu_lock = true;
+		$(".menu_bar").removeClass("rotate");
+		$("#menu_white_background").fadeOut(300);
+		$("#header_logo_wrapper h1").removeClass("active");
+		$("#side_navigation").removeClass("behind");
+		$("#menu .hexagon,#menu #menu_header_circle,#menu .line_1, #menu .line_2, #menu .line_3, #menu h3, #menu h2").removeClass("active");
+		setTimeout(function() {
+			$('#menu_tree').css('display', 'none');
+			menu_lock = false;
+		}, 300);
+	}else{
+		menu_open = true;
+		$('#menu_tree').css('display', 'block');
+		$(".menu_bar").addClass("rotate");
+		$("#menu_white_background").fadeIn(300);
+		$("#header_logo_wrapper h1").addClass("active");
+		$("#side_navigation").addClass("behind");
+		$("#menu .hexagon, #menu #menu_header_circle,#menu .line_1, #menu .line_2, #menu .line_3, #menu h3, #menu h2").addClass("active");
+	}
+}
+
+function openPage( page_number ){
+	setTimeout( function(){
+		current_page = page_number;
+		$("#page_" + page_number).addClass("active_page");
+	}, 1000)
+}
+
+function closePage( page_number ){
+	lock = true;
+	setTimeout(unlock, 1200);
+	$("#page_" + page_number).removeClass("active_page");
+}
+
+function jumpToPage( page_number ){
+	if(lock || (page_number == current_page)) return;
+	closePage(current_page);
+	if(menu_open == true){
+		toggleMenu();
+	}
+	if( page_number == 1){
+		$("body").css("background-color", "#0a236b");
+		$("#header_logo_wrapper h1").addClass("hidden");
+		$("#menu_icon img, #header_logo_wrapper img").removeClass("active");
+		$("#menu_icon #icon_1").addClass("active");
+		$('.side_circle').removeClass("active");
+		$('#side_circle_1').addClass("active");
+		openPage(1);
+	}else if( page_number == 2 ){
+		$("body").css("background-color", "#38c2a6");
+		$("#header_logo_wrapper h1").removeClass("hidden");
+		$("#menu_icon img, #header_logo_wrapper img").removeClass("active");
+		$("#menu_icon #icon_2, #header_logo_wrapper #header_logo_2").addClass("active");
+		$('.side_circle').removeClass("active");
+		$('#side_circle_2').addClass("active");
+		openPage(2);
+	}else if( page_number == 3 ){
+		$("body").css("background-color", page_3_color);
+		$("#header_logo_wrapper h1").removeClass("hidden");
+		$("#menu_icon img, #header_logo_wrapper img").removeClass("active");
+		$("#menu_icon #icon_3, #header_logo_wrapper #header_logo_3").addClass("active");
+		$('.side_circle').removeClass("active");
+		$('#side_circle_3').addClass("active");
+		openPage(3);
+	}else if( page_number == 4 ){
+		$("body").css("background-color", "#9e8fdd");
+		$("#header_logo_wrapper h1").removeClass("hidden");
+		$("#menu_icon img, #header_logo_wrapper img").removeClass("active");
+		$("#menu_icon #icon_4, #header_logo_wrapper #header_logo_4").addClass("active");
+		$('.side_circle').removeClass("active");
+		$('#side_circle_4').addClass("active");
+		openPage(4);
+	}else if( page_number == 5 ){
+		$("body").css("background-color", "#4b816f");
+		$("#header_logo_wrapper h1").removeClass("hidden");
+		$("#menu_icon img, #header_logo_wrapper img").removeClass("active");
+		$("#menu_icon #icon_5, #header_logo_wrapper #header_logo_5").addClass("active");
+		$('.side_circle').removeClass("active");
+		$('#side_circle_5').addClass("active");
+		openPage(5);
+	}
+}
+
+function changeDesignPreview( slide_number ){
+	if( slide_number == 1){
+		$(".design_slide").removeClass("active");
+		$("#design_slide_1").addClass("active");
+	}else if( slide_number == 2 ){
+		$(".design_slide").removeClass("active");
+		$("#design_slide_2").addClass("active");
+	}else if( slide_number == 3 ){
+		$(".design_slide").removeClass("active");
+		$("#design_slide_3").addClass("active");
+	}else if( slide_number == 4 ){
+		$(".design_slide").removeClass("active");
+		$("#design_slide_4").addClass("active");
+	}else if( slide_number == 5 ){
+		$(".design_slide").removeClass("active");
+		$("#design_slide_5").addClass("active");
+	}
+}
+function changeWebPreview( slide_number ){
+	if( slide_number == 1){
+		$(".web_slide").removeClass("active");
+		$("#web_slide_1").addClass("active");
+		page_3_color = "#f0af3e";
+		$("body").css("background-color", page_3_color);
+		$(".web_slide .shadow_box").css("background-color", page_3_color);
+	}else if( slide_number == 2 ){
+		//FIG
+		$(".web_slide").removeClass("active");
+		$("#web_slide_2").addClass("active");
+		page_3_color = "#4774c5";
+		$("body").css("background-color", page_3_color);
+		$(".web_slide .shadow_box").css("background-color", page_3_color);
+	}else if( slide_number == 3 ){
+		//urbanHive
+		$(".web_slide").removeClass("active");
+		$("#web_slide_3").addClass("active");
+		page_3_color = "#fbde02";
+		$("body").css("background-color", page_3_color);
+		$(".web_slide .shadow_box").css("background-color", page_3_color);
+	}else if( slide_number == 4 ){
+		//Neep
+		$(".web_slide").removeClass("active");
+		$("#web_slide_4").addClass("active");
+		page_3_color = "#be80bd";
+		$("body").css("background-color", page_3_color);
+		$(".web_slide .shadow_box").css("background-color", page_3_color);
+	}else if( slide_number == 5 ){
+		//Rowing
+		$(".web_slide").removeClass("active");
+		$("#web_slide_5").addClass("active");
+		page_3_color = "#604c4c";
+		$("body").css("background-color", page_3_color);
+		$(".web_slide .shadow_box").css("background-color", page_3_color);
+	}else if( slide_number == 6 ){
+		//Rohlfs
+		$(".web_slide").removeClass("active");
+		$("#web_slide_6").addClass("active");
+		page_3_color = "#631b1d";
+		$("body").css("background-color", page_3_color);
+		$(".web_slide .shadow_box").css("background-color", page_3_color);
+	}
+}
+function changeDesignRight(){
+	page_4_slide++;
+	if(page_4_slide > page_4_max){
+		page_4_slide = 1;
+	}
+	changeDesignPreview(page_4_slide);
+}
+function changeDesignLeft(){
+	page_4_slide--;
+	if(page_4_slide < 1){
+		page_4_slide = page_4_max;
+	}
+	changeDesignPreview(page_4_slide);
+}
+function changeWebRight(){
+	page_3_slide++;
+	if(page_3_slide > page_3_max){
+		page_3_slide = 1;
+	}
+	changeWebPreview(page_3_slide);
+}
+function changeWebLeft(){
+	page_3_slide--;
+	if(page_3_slide < 1){
+		page_3_slide = page_3_max;
+	}
+	changeWebPreview(page_3_slide);
+}
+
+function scrollUp(){
+	if(current_page > 1){
+		jumpToPage(current_page - 1);
+	}
+}
+function scrollDown(){
+	if(current_page < max_pages){
+		jumpToPage(current_page + 1);
+	}
+}
+
+$(window).bind('mousewheel DOMMouseScroll', function(event){
+    if(lock || menu_open) return;
+	if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+		scrollUp();
+    }
+    else {
+		scrollDown();
+    }
+});
+
+$(document).keydown(function(e) {
+	if(lock || menu_open || $("input,textarea").is(":focus")) return;
+	switch(e.which) {
+		case 38: // up
+			scrollUp();
+			break;
+		case 40: // down
+			scrollDown();
+			break;
+			
+		case 37: // left
+			if(current_page == 3) changeWebLeft(); 
+			if(current_page == 4) changeDesignLeft(); 
+			break;
+		case 39: // right
+			if(current_page == 3) changeWebRight(); 
+			if(current_page == 4) changeDesignRight(); 
+			break;
+			
+		default: return;
+	}
+});
+
+$(function(){
+	jumpToPage(2);
+	//changeWebRight();
+});
